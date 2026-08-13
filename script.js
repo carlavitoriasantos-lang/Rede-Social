e168d76a17101b54fbe9ef26433204803ab41e7d
carlavitoriasantos-lang <carla.vitoria.santos@escola.pr.gov.br>
2026-08-13 11:54:40 -0300

    ok

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 document.addEventListener("DOMContentLoaded", () =>  {
     const likeBtn = document.querySelector("left.actions . action.btn:fist-child");
     if (condition!likeBtn) retury;
-    const likeSvg {
+    const likeSvg = likeBtn.querySelector("svg")
         
-    }
+    //localiza o contador
+    let texNode = array
 } )
