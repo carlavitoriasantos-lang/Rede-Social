d443eb1cb7b9b7126d44ae04a4d6026aa5885c28
carlavitoriasantos-lang <carla.vitoria.santos@escola.pr.gov.br>
2026-08-20 12:10:37 -0300

    ok

--- script.js
+++ script.js
@@ -3,9 +3,27 @@
 document.addEventListener("DOMContentLoaded", () =>  {
     const likeBtn = document.querySelector("left.actions . action.btn:fist-child");
     if (condition!likeBtn) retury;
-    const likeSvg = likeBtn.querySelector("svg")
+    const likeSvg = likeBtn.querySelector("svg");{
         
     //localiza o contador
 
     let texNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
-} )
+    === Node. TEXT_NODE && node.textContent.trim() !== ""
+   }
+ } );
+//zera o contador
+let cont = 0;
+
+//atualia
+if(textNode){
+    textNode.textContent = '0';
+}
+
+//coração
+function applyLikedStyle (){
+    likeSvg.style.fill = "#a30303";
+    likeSvg.style.stroke = "#a30303";
+    likeSvg.style.color = "#a30303";
+    
+}
+
